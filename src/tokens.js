9d44df773d480dab4584e53bf28745cb182f893c
anthologen <anthologen@gmail.com>
2021-12-24 23:50:58 -0500

    update to use @lezer/lr module

--- src/tokens.js
+++ src/tokens.js
@@ -1,21 +1,16 @@
-import { ExternalTokenizer } from "lezer"
-import { omitSemi } from "./parser.terms.js"
+import { ExternalTokenizer, ContextTracker } from "@lezer/lr"
+import { insertSemi, spaces, newline, LineComment, BlockComment } from "./parser.terms.js"
 
-const newline = [10, 13, 8232, 8233]
-const space = [9, 11, 12, 32, 133, 160, 5760, 8192, 8193, 8194, 8195, 8196, 8197, 8198, 8199, 8200, 8201, 8202, 8239, 8287, 12288]
-const braceR = 125
+export const trackNewline = new ContextTracker({
+  start: false,
+  shift(context, term) {
+    return term == LineComment || term == BlockComment || term == spaces ? context : term == newline
+  },
+  strict: false
+})
 
-function newlineBefore(input, pos) {
-  for (let i = pos - 1; i >= 0; i--) {
-    let prev = input.get(i)
-    if (newline.indexOf(prev) > -1) return true
-    if (space.indexOf(prev) < 0) break
-  }
-  return false
-}
-
-export const omitSemicolon = new ExternalTokenizer((input, token, stack) => {
-  let pos = token.start, next = input.get(pos)
-  if ((next == braceR || next == -1 || newlineBefore(input, pos)) && stack.canShift(omitSemi))
-    token.accept(omitSemi, token.start)
+export const insertSemicolon = new ExternalTokenizer((input, stack) => {
+  let {next} = input
+  if ((next == -1 || stack.context) && stack.canShift(insertSemi))
+    input.acceptToken(insertSemi)
 }, {contextual: true, fallback: true})
